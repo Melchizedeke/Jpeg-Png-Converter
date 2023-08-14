c724b8a9eea05e052c445a94010fd0518be3056f
Igoche John Eigege <140503808+Melchizedeke@users.noreply.github.com>
2023-08-14 19:31:08 +0100

    modification
    modified my code by adding some new lines and tested it, worked well

---
+++ converter.py
@@ -0,0 +1,11 @@
+# Basic Library Imports
+import os
+import sys
+from PIL import Image
+
+# Grab the first and second arguments as from the command line
+main_folder = sys.argv[1]
+new_folder = sys.argv[2]
+
+# print to test if it works well
+print(main_folder, new_folder)
